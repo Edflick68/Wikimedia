a1e40986a6b91cf551b5415c7ea7a5e1b26c7359
Potated <seanedwarddubois@gmail.com>
2026-03-18 02:20:18 -0400

    points 1 et 2 (model media)

--- Models/Media.cs
+++ Models/Media.cs
@@ -1,20 +1,20 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace Wikimedia.Models
 {
-    public class Media
+    public class Media : Record
     {
-        public int Id { get; set; }
-        public string Title { get; set; } 
+        public string Title { get; set; }
         public string Category { get; set; }
-        public string CategoryId { get; set; }
-        public string Description { get; set; } 
+        public string Description { get; set; }
         public string YoutubeId { get; set; }
         public DateTime PublishDate { get; set; } = DateTime.Now;
-
-        public DateTime LastModified {  get; set; } 
+        public DateTime LastModified { get; set; }
+        public int UserId {  get; set; }
+        public bool Shared { get; set; }
     }
 }
